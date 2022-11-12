f507424af80caf1ba158caa1d7797b910565fa4b
MauriellGarcia <116768292+MauriellGarcia@users.noreply.github.com>
2022-11-11 18:17:55 -0600

    Add files via upload

---
+++ CiudadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciudad.ciudad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiudadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
